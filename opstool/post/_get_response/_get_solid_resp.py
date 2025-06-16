88b28fb2bdc49eea0b915dc905966fa89ed213e2
Yexiang Yan <yexiang_yan@outlook.com>
2025-06-15 20:15:16 -0400

    feat: Added the function of projecting Gaussian point response to nodes

--- opstool/post/_get_response/_get_solid_resp.py
+++ opstool/post/_get_response/_get_solid_resp.py
@@ -1,19 +1,30 @@
+from collections import defaultdict
 from typing import Optional
 
 import numpy as np
 import openseespy.opensees as ops
 import xarray as xr
 
+from ...utils import get_gp2node_func
 from ._response_base import ResponseBase, _expand_to_uniform_array
-
-# from ...utils import OPS_ELE_TAGS
 
 
 class BrickRespStepData(ResponseBase):
-    def __init__(self, ele_tags=None, compute_measures: bool = True, model_update: bool = False, dtype: dict = None):
+    def __init__(
+        self,
+        ele_tags=None,
+        compute_measures: bool = True,
+        compute_nodal_resp: Optional[str] = None,
+        model_update: bool = False,
+        dtype: Optional[dict] = None,
+    ):
         self.resp_names = [
             "Stresses",
             "Strains",
+            "StressesAtNodes",
+            "StressAtNodesErr",
+            "StrainsAtNodes",
+            "StrainsAtNodesErr",
         ]
         self.resp_steps = None
         self.resp_steps_list = []  # for model update
@@ -23,6 +34,9 @@ class BrickRespStepData(ResponseBase):
         self.times = []
 
         self.compute_measures = compute_measures
+        self.compute_nodal_resp = compute_nodal_resp
+        self.nodal_resp_method = compute_nodal_resp
+        self.node_tags = None
         self.model_update = model_update
         self.dtype = {"int": np.int32, "float": np.float32}
         if isinstance(dtype, dict):
@@ -59,6 +73,15 @@ class BrickRespStepData(ResponseBase):
     def add_data_one_step(self, ele_tags):
         stresses, strains = _get_gauss_resp(ele_tags, dtype=self.dtype)
 
+        if self.compute_nodal_resp:
+            node_stress_avg, node_stress_rel_error, node_tags = _get_nodal_resp(
+                ele_tags, stresses, method=self.nodal_resp_method, dtype=self.dtype
+            )
+            node_strain_avg, node_strain_rel_error, node_tags = _get_nodal_resp(
+                ele_tags, strains, method=self.nodal_resp_method, dtype=self.dtype
+            )
+            self.node_tags = node_tags
+
         if self.stressDOFs is None:
             if stresses.shape[-1] == 6:
                 self.stressDOFs = ["sigma11", "sigma22", "sigma33", "sigma12", "sigma23", "sigma13"]
@@ -73,25 +96,34 @@ class BrickRespStepData(ResponseBase):
             data_vars = {}
             data_vars["Stresses"] = (["eleTags", "GaussPoints", "stressDOFs"], stresses)
             data_vars["Strains"] = (["eleTags", "GaussPoints", "strainDOFs"], strains)
-            ds = xr.Dataset(
-                data_vars=data_vars,
-                coords={
-                    "eleTags": ele_tags,
-                    "GaussPoints": self.GaussPoints,
-                    "stressDOFs": self.stressDOFs,
-                    "strainDOFs": self.strainDOFs,
-                },
-                attrs=self.attrs,
-            )
+            coords = {
+                "eleTags": ele_tags,
+                "GaussPoints": self.GaussPoints,
+                "stressDOFs": self.stressDOFs,
+                "strainDOFs": self.strainDOFs,
+            }
+            if self.compute_nodal_resp:
+                data_vars["StressesAtNodes"] = (["nodeTags", "stressDOFs"], node_stress_avg)
+                data_vars["StrainsAtNodes"] = (["nodeTags", "strainDOFs"], node_strain_avg)
+                data_vars["StressAtNodesErr"] = (["nodeTags", "stressDOFs"], node_stress_rel_error)
+                data_vars["StrainsAtNodesErr"] = (["nodeTags", "strainDOFs"], node_strain_rel_error)
+                coords["nodeTags"] = node_tags
+            ds = xr.Dataset(data_vars=data_vars, coords=coords, attrs=self.attrs)
             self.resp_steps_list.append(ds)
         else:
             self.resp_steps_dict["Stresses"].append(stresses)
             self.resp_steps_dict["Strains"].append(strains)
+            if self.compute_nodal_resp:
+                self.resp_steps_dict["StressesAtNodes"].append(node_stress_avg)
+                self.resp_steps_dict["StrainsAtNodes"].append(node_strain_avg)
+                self.resp_steps_dict["StressAtNodesErr"].append(node_stress_rel_error)
+                self.resp_steps_dict["StrainsAtNodesErr"].append(node_strain_rel_error)
 
         self.times.append(ops.getTime())
         self.step_track += 1
 
     def _to_xarray(self):
+        self.times = np.array(self.times, dtype=self.dtype["float"])
         if self.model_update:
             self.resp_steps = xr.concat(self.resp_steps_list, dim="time", join="outer")
             self.resp_steps.coords["time"] = self.times
@@ -99,17 +131,32 @@ class BrickRespStepData(ResponseBase):
             data_vars = {}
             data_vars["Stresses"] = (["time", "eleTags", "GaussPoints", "stressDOFs"], self.resp_steps_dict["Stresses"])
             data_vars["Strains"] = (["time", "eleTags", "GaussPoints", "strainDOFs"], self.resp_steps_dict["Strains"])
-            self.resp_steps = xr.Dataset(
-                data_vars=data_vars,
-                coords={
-                    "time": self.times,
-                    "eleTags": self.ele_tags,
-                    "GaussPoints": self.GaussPoints,
-                    "stressDOFs": self.stressDOFs,
-                    "strainDOFs": self.strainDOFs,
-                },
-                attrs=self.attrs,
-            )
+            coords = {
+                "time": self.times,
+                "eleTags": self.ele_tags,
+                "GaussPoints": self.GaussPoints,
+                "stressDOFs": self.stressDOFs,
+                "strainDOFs": self.strainDOFs,
+            }
+            if self.compute_nodal_resp:
+                data_vars["StressesAtNodes"] = (
+                    ["time", "nodeTags", "stressDOFs"],
+                    self.resp_steps_dict["StressesAtNodes"],
+                )
+                data_vars["StrainsAtNodes"] = (
+                    ["time", "nodeTags", "strainDOFs"],
+                    self.resp_steps_dict["StrainsAtNodes"],
+                )
+                data_vars["StressAtNodesErr"] = (
+                    ["time", "nodeTags", "stressDOFs"],
+                    self.resp_steps_dict["StressAtNodesErr"],
+                )
+                data_vars["StrainsAtNodesErr"] = (
+                    ["time", "nodeTags", "strainDOFs"],
+                    self.resp_steps_dict["StrainsAtNodesErr"],
+                )
+                coords["nodeTags"] = self.node_tags
+            self.resp_steps = xr.Dataset(data_vars=data_vars, coords=coords, attrs=self.attrs)
 
         if self.compute_measures:
             self._compute_measures_()
@@ -141,6 +188,25 @@ class BrickRespStepData(ResponseBase):
             coords=coords,
             name="strainMeasures",
         )
+        if self.compute_nodal_resp:
+            node_stress_measures = _calculate_stresses_measures_4D(
+                self.resp_steps["StressesAtNodes"].data, dtype=self.dtype
+            )
+            node_strain_measures = _calculate_stresses_measures_4D(
+                self.resp_steps["StrainsAtNodes"].data, dtype=self.dtype
+            )
+            dims = ["time", "nodeTags", "measures"]
+            coords = {
+                "time": stresses.coords["time"],
+                "nodeTags": self.resp_steps["StressesAtNodes"].coords["nodeTags"],
+                "measures": ["p1", "p2", "p3", "sigma_vm", "tau_max", "sigma_oct", "tau_oct"],
+            }
+            self.resp_steps["StressMeasuresAtNodes"] = xr.DataArray(
+                node_stress_measures, dims=dims, coords=coords, name="StressMeasuresAtNodes"
+            )
+            self.resp_steps["StrainMeasuresAtNodes"] = xr.DataArray(
+                node_strain_measures, dims=dims, coords=coords, name="StrainMeasuresAtNodes"
+            )
 
     def get_data(self):
         return self.resp_steps
@@ -168,7 +234,10 @@ class BrickRespStepData(ResponseBase):
             {"stressDOFs": ["sigma11", "sigma22", "sigma33", "sigma12", "sigma23", "sigma13"]}
         ] *= stress_factor
 
-        resp_steps["stressMeasures"] *= stress_factor
+        if "stressMeasures" in resp_steps.data_vars:
+            resp_steps["stressMeasures"] *= stress_factor
+        if "StressMeasuresAtNodes" in resp_steps.data_vars:
+            resp_steps["StressMeasuresAtNodes"] *= stress_factor
 
         return resp_steps
 
@@ -191,6 +260,50 @@ class BrickRespStepData(ResponseBase):
                 return ds[resp_type]
 
 
+gp2node_type = {4: "tet", 10: "tet", 8: "brick", 20: "brick", 27: "brick"}
+
+
+# Get nodal stresses and strains from the Gauss points of elements.
+def _get_nodal_resp(ele_tags, ele_gp_resp, method, dtype):
+    node_resp = defaultdict(list)
+    for etag, gp_resp in zip(ele_tags, ele_gp_resp):
+        etag = int(etag)
+        ntags = ops.eleNodes(etag)
+        gp_resp = gp_resp[~np.isnan(gp_resp).any(axis=1)]
+        if len(gp_resp) == 0:
+            continue
+        gp2node_func = get_gp2node_func(ele_type=gp2node_type[len(ntags)], n=len(ntags), gp=len(gp_resp))
+        if gp2node_func:
+            resp = gp2node_func(method=method, gp_resp=gp_resp)
+        else:
+            resp = np.zeros((len(ntags), gp_resp.shape[1]), dtype=dtype["float"])
+        for i, ntag in enumerate(ntags):
+            node_resp[ntag].append(resp[i])
+    # node_resp = dict(sorted(node_resp.items()))
+    node_avg = {}
+    # node_max = {}
+    # node_min = {}
+    node_ptp = {}  # Peak-to-peak: max - min
+    # node_std = {}
+    node_rel_error = {}
+
+    for nid, vals in node_resp.items():
+        arr = np.stack(vals, axis=0)  # shape: (k, m), k=num_samples, m=DOFs
+        node_avg[nid] = np.nanmean(arr, axis=0)  # mean value
+        # node_max[nid] = np.nanmax(arr, axis=0)  # maximum value
+        # node_min[nid] = np.nanmin(arr, axis=0)  # minimum value
+        node_ptp[nid] = np.nanmax(arr, axis=0) - np.nanmin(arr, axis=0)
+        # node_std[nid] = np.nanstd(arr, axis=0)  # standard deviation
+
+        node_rel_error[nid] = node_ptp[nid] / (np.abs(node_avg[nid]) + 1e-8)  # avoid division by zero
+        node_rel_error[nid][np.abs(node_avg[nid]) < 1e-8] = 0.0  # if avg is close to zero, set rel error to zero
+    node_avg = np.array(list(node_avg.values()), dtype=dtype["float"])
+    node_rel_error = np.array(list(node_rel_error.values()), dtype=dtype["float"])
+    node_tags = list(node_resp.keys())
+    return node_avg, node_rel_error, node_tags
+
+
+#  Get Gauss point stresses and strains for all elements in the model.
 def _get_gauss_resp(ele_tags, dtype: dict):
     all_stresses, all_strains = [], []
     for etag in ele_tags:
@@ -312,26 +425,48 @@ def _assemble_stress_tensor_4D(stress_array):
     Returns:
         np.ndarray: shape (time, eleTags, GaussPoints, 3, 3)
     """
-    num_time, num_elements, num_gauss_points, _ = stress_array.shape
-    stress_tensor = np.full((num_time, num_elements, num_gauss_points, 3, 3), np.nan)
+    if stress_array.ndim == 4:
+        num_time, num_elements, num_gauss_points, _ = stress_array.shape
+        stress_tensor = np.full((num_time, num_elements, num_gauss_points, 3, 3), np.nan)
 
-    for t in range(num_time):
-        for i in range(num_elements):
-            for j in range(num_gauss_points):
-                sig11 = stress_array[t, i, j, 0]
-                sig22 = stress_array[t, i, j, 1]
-                sig33 = stress_array[t, i, j, 2]
-                tau12 = stress_array[t, i, j, 3]
-                tau23 = stress_array[t, i, j, 4]
-                tau13 = stress_array[t, i, j, 5]
+        for t in range(num_time):
+            for i in range(num_elements):
+                for j in range(num_gauss_points):
+                    sig11 = stress_array[t, i, j, 0]
+                    sig22 = stress_array[t, i, j, 1]
+                    sig33 = stress_array[t, i, j, 2]
+                    tau12 = stress_array[t, i, j, 3]
+                    tau23 = stress_array[t, i, j, 4]
+                    tau13 = stress_array[t, i, j, 5]
+
+                    if np.any(np.isnan([sig11, sig22, sig33, tau12, tau23, tau13])):
+                        continue  # skip invalid tensor
+
+                    stress_tensor[t, i, j, ...] = np.array([
+                        [sig11, tau12, tau13],
+                        [tau12, sig22, tau23],
+                        [tau13, tau23, sig33],
+                    ])
+    elif stress_array.ndim == 3:
+        num_time, num_nodes, _ = stress_array.shape
+        stress_tensor = np.full((num_time, num_nodes, 3, 3), np.nan)
+        for t in range(num_time):
+            for i in range(num_nodes):
+                sig11 = stress_array[t, i, 0]
+                sig22 = stress_array[t, i, 1]
+                sig33 = stress_array[t, i, 2]
+                tau12 = stress_array[t, i, 3]
+                tau23 = stress_array[t, i, 4]
+                tau13 = stress_array[t, i, 5]
 
                 if np.any(np.isnan([sig11, sig22, sig33, tau12, tau23, tau13])):
-                    continue  # skip invalid tensor
-
-                stress_tensor[t, i, j, ...] = np.array([
+                    continue
+                stress_tensor[t, i, ...] = np.array([
                     [sig11, tau12, tau13],
                     [tau12, sig22, tau23],
                     [tau13, tau23, sig33],
                 ])
+    else:
+        raise ValueError(f"Invalid stress_array shape: {stress_array.shape}. Expected 3D or 4D array.")  # noqa: TRY003
 
     return np.nan_to_num(stress_tensor, nan=0.0)
